3ee4320399f516323faabc8bc452b484b41b41c9
yoneya <Yoneya.ohm1221@gmail.com>
2021-11-18 14:25:58 +0700

    edit

--- app/src/main/java/com/example/phoneblockerproject/MainActivity.kt
+++ app/src/main/java/com/example/phoneblockerproject/MainActivity.kt
@@ -1,15 +1,24 @@
 package com.example.phoneblockerproject
 
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf( android.Manifest.permission.READ_PHONE_STATE,android.Manifest.permission.READ_CALL_LOG),1 )
+        }
 
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
