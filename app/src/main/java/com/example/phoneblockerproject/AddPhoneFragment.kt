82c541fd2ffb96ba1ce76ded8b2b748417c5b1e3
yoneya <Yoneya.ohm1221@gmail.com>
2021-11-22 10:49:00 +0700

    commit

--- app/src/main/java/com/example/phoneblockerproject/AddPhoneFragment.kt
+++ app/src/main/java/com/example/phoneblockerproject/AddPhoneFragment.kt
@@ -9,14 +9,17 @@ import android.view.ViewGroup
 
 class AddPhoneFragment : Fragment() {
 
-
+    val data = ArrayList<Data>()
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         var root = inflater.inflate(R.layout.fragment_add_phone, container, false)
-
+        data.add(Data("1","โจร","0222222"))
+        data.add(Data("2","โจร2","0333333"))
 
         return root
     }
-
+    class Data(
+            var id: String,  var name: String,  var phoneNumber: String
+    )
 }
--- app/src/main/java/com/example/phoneblockerproject/MyService.kt
+++ app/src/main/java/com/example/phoneblockerproject/MyService.kt
@@ -1,12 +1,24 @@
 package com.example.phoneblockerproject
 
+import android.Manifest
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.media.AudioManager
+import android.os.Build
 import android.os.IBinder
+import android.telecom.TelecomManager
+import android.telephony.TelephonyManager
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+
 
 class MyService : Service() {
 
     override fun onBind(intent: Intent): IBinder? {
       return  null
     }
+
 }
