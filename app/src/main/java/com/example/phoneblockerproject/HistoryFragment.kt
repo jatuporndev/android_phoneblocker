d1853d5b508035533728452f0b3922f0e20e5fe3
suwatnan <suwatnan2543@gmail.com>
2021-11-23 12:28:00 +0700

    หน้า HIS

--- app/src/main/java/com/example/phoneblockerproject/HistoryFragment.kt
+++ app/src/main/java/com/example/phoneblockerproject/HistoryFragment.kt
@@ -1,15 +1,17 @@
 package com.example.phoneblockerproject
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
 
 
 class HistoryFragment : Fragment() {
-
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
