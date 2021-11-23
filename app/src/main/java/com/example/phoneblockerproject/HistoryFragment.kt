9eae1d9ad136a99ca4ab41c133cc037349ca25e5
yoneya <Yoneya.ohm1221@gmail.com>
2021-11-23 10:15:25 +0700

    history

--- app/src/main/java/com/example/phoneblockerproject/HistoryFragment.kt
+++ app/src/main/java/com/example/phoneblockerproject/HistoryFragment.kt
@@ -9,14 +9,18 @@ import android.view.ViewGroup
 
 class HistoryFragment : Fragment() {
 
-
+    val data = ArrayList<Data>()
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         var root =  inflater.inflate(R.layout.fragment_history, container, false)
+        data.add((Data("1","โจร","5555","0")))
 
         return root
     }
+    class Data(
+            var id: String,  var name: String,  var phoneNumber: String, var statsu: String
+    )
 
 
 
--- app/src/main/java/com/example/phoneblockerproject/databass/DBHelper.kt
+++ app/src/main/java/com/example/phoneblockerproject/databass/DBHelper.kt
@@ -11,47 +11,60 @@ class DBHelper(context: Context): SQLiteOpenHelper(context,DATABASE_NAME,null,DA
     companion object {
          val DATABASE_VERSION = 1
          val DATABASE_NAME = "Database"
-         val TABLE_CONTACTS = "phoneblocker"
-         val KEY_ID = "id"
-         val KEY_NAME = "name"
-         val KEY_PHONE = "phoneNumber"
+
+        //Table Phone adding
+         val TABLE_PHONEBLOCKER = "phoneblocker"
+         val PHONEBLOCKER_ID = "id"
+         val PHONEBLOCKER_NAME = "name"
+         val PHONEBLOCKER_PHONE = "phoneNumber"
+
+        //Table Phone spaming
+        val TABLE_PHONESPAMER = "phonespamer"
+        val PHONESPAMER_ID = "id"
+        val PHONESPAMER_NAME = "name"
+        val PHONESPAMER_PHONE = "phoneNumber"
+
+        //Table Phone history
+        val TABLE_PHONEHISTORY = "phonespamer"
+        val PHONEHISTORY_ID = "id"
+        val PHONEHISTORY_NAME = "name"
+        val PHONEHISTORY_PHONE = "phoneNumber"
     }
+
     override fun onCreate(db: SQLiteDatabase?) {
-        val CREATE_CONTACTS_TABLE = ("CREATE TABLE " + TABLE_CONTACTS + "("
-                + KEY_ID + " INTEGER PRIMARY KEY," + KEY_NAME + " TEXT,"
-                + KEY_PHONE + " TEXT" + ")")
+        val CREATE_CONTACTS_TABLE = ("CREATE TABLE " + TABLE_PHONEBLOCKER + "("
+                + PHONEBLOCKER_ID + " INTEGER PRIMARY KEY," + PHONEBLOCKER_NAME + " TEXT,"
+                + PHONEBLOCKER_PHONE + " TEXT" + ")")
         db?.execSQL(CREATE_CONTACTS_TABLE)
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
-        db!!.execSQL("DROP TABLE IF EXISTS " + TABLE_CONTACTS)
+        db!!.execSQL("DROP TABLE IF EXISTS " + TABLE_PHONEBLOCKER)
         onCreate(db)
     }
     fun addPhone(name : String, phone : String ){
 
         val values = ContentValues()
-        values.put(KEY_NAME, name)
-        values.put(KEY_PHONE, phone)
+        values.put(PHONEBLOCKER_NAME, name)
+        values.put(PHONEBLOCKER_PHONE, phone)
         val db = this.writableDatabase
 
-        db.insert(TABLE_CONTACTS, null, values)
+        db.insert(TABLE_PHONEBLOCKER, null, values)
         db.close()
     }
     fun deletePhone(id:String){
 
         val db = this.writableDatabase
-        db.execSQL("delete from "+ TABLE_CONTACTS +" WHERE id ="+id);
+        db.execSQL("delete from "+ TABLE_PHONEBLOCKER +" WHERE id ="+id);
     }
-
 
     fun getPhone(phone:String): Cursor? {
-
         val db = this.readableDatabase
-
         if (phone=="" || phone == null){
-            return db.rawQuery("SELECT * FROM "+TABLE_CONTACTS, null)
+            return db.rawQuery("SELECT * FROM "+TABLE_PHONEBLOCKER, null)
         }else{
-            return db.rawQuery("SELECT * FROM "+TABLE_CONTACTS+" WHERE ($KEY_NAME LIKE '%"+ phone+"%' or $KEY_PHONE LIKE '%"+phone+"%')", null)
+            return db.rawQuery("SELECT * FROM "+TABLE_PHONEBLOCKER+" WHERE ($PHONEBLOCKER_NAME LIKE '%"+ phone+"%' or $PHONEBLOCKER_PHONE LIKE '%"+phone+"%')", null)
         }
     }
+
 }
