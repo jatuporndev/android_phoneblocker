ec613cc680e122dd083456fd9363d8ed491bfb46
yoneya <Yoneya.ohm1221@gmail.com>
2021-11-24 12:17:56 +0700

    history update

--- app/src/main/java/com/example/phoneblockerproject/Detector/CallReciver.kt
+++ app/src/main/java/com/example/phoneblockerproject/Detector/CallReciver.kt
@@ -15,14 +15,20 @@ import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import com.example.phoneblockerproject.databass.DBHelper
 
-
+@RequiresApi(Build.VERSION_CODES.LOLLIPOP)
 class CallReciver : BroadcastReceiver() {
     val data = ArrayList<Data>()
-    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+
     override fun onReceive(context: Context?, intent: Intent?) {
 
         allPhone(context)
+        callblocker(context,intent)
 
+    }
+
+
+
+    fun callblocker(context: Context?, intent: Intent?){
         if (intent?.getStringExtra(TelephonyManager.EXTRA_STATE)!! == TelephonyManager.EXTRA_STATE_RINGING) {
             var incomingNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER)
             if (data.any { it.phoneNumber == incomingNumber }) {
@@ -31,12 +37,11 @@ class CallReciver : BroadcastReceiver() {
 
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
                     if (ActivityCompat.checkSelfPermission(
-                            context,
-                            Manifest.permission.ANSWER_PHONE_CALLS
-                        ) != PackageManager.PERMISSION_GRANTED) {
+                                    context,
+                                    Manifest.permission.ANSWER_PHONE_CALLS
+                            ) != PackageManager.PERMISSION_GRANTED) {
                         return
                     }
-                    //Toast.makeText(context, "ระบุเบอร์!!", Toast.LENGTH_LONG).show()
                     telecomManager.endCall()
 
                 }
@@ -44,6 +49,9 @@ class CallReciver : BroadcastReceiver() {
             }
         }
     }
+
+
+
 
     class Data(var id: String, var name: String, var phoneNumber: String)
     @SuppressLint("Range")
