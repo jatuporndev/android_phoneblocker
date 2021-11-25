651cb3f3700c6ab5379dc4d5c53380ec1084b7db
suwatnan <suwatnan2543@gmail.com>
2021-11-25 10:37:14 +0700

    phone edit

--- app/src/main/java/com/example/phoneblockerproject/HistoryPhoneFragment.kt
+++ app/src/main/java/com/example/phoneblockerproject/HistoryPhoneFragment.kt
@@ -1,33 +1,88 @@
 package com.example.phoneblockerproject
 
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.RecyclerView
+import com.example.phoneblockerproject.Detector.CallReciver
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class HistoryPhoneFragment : Fragment() {
-    var imgbackc: ImageView?=null
-
+    var imgbackc: ImageView? = null
+    var data = ArrayList<Data>()
+    var recyclerView: RecyclerView? = null
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        var root =  inflater.inflate(R.layout.fragment_history_phone, container, false)
+        var root = inflater.inflate(R.layout.fragment_history_phone, container, false)
+        data.add((Data("1","man","095-773-9456","0","25/11/2564" )))
+        recyclerView = root.findViewById(R.id.recyclerView)
+        recyclerView!!.adapter = DataAdapter(data)
 
         imgbackc = root.findViewById(R.id.imgbackc)
-        imgbackc?.setOnClickListener{
-            val fragmentTransaction = requireActivity().
-            supportFragmentManager.beginTransaction()
+        imgbackc?.setOnClickListener {
+            val fragmentTransaction = requireActivity().supportFragmentManager.beginTransaction()
             fragmentTransaction.replace(R.id.nav_host_fragment, HistoryFragment())
             fragmentTransaction.addToBackStack(null)
             fragmentTransaction.commit()
 
         }
         return root
-    }
 
+
+    }
+    class Data(
+            var id: String,
+            var name: String,
+            var phonenember: String,
+            var status: String,
+            var date: String
+    )
+    internal inner class DataAdapter(private val list: List<Data>) :
+            RecyclerView.Adapter<DataAdapter.ViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val view: View = LayoutInflater.from(parent.context).inflate(
+                    R.layout.item_blockphone,
+                    parent, false
+            )
+            return ViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            Log.d("txt", "x3")
+            val data = list[position]
+            holder.data = data
+            holder.txtname.text = data.name
+            holder.txtphone.text = data.phonenember
+            holder.txtdate.text = data.date
+
+
+        }
+
+
+        override fun getItemCount(): Int {
+            return list.size
+        }
+
+        internal inner class ViewHolder(itemView: View) :
+                RecyclerView.ViewHolder(itemView) {
+
+            var data: Data? = null
+            var txtname: TextView = itemView.findViewById(R.id.txtname)
+            var txtphone: TextView = itemView.findViewById(R.id.txtphone)
+            var txtdate: TextView = itemView.findViewById(R.id.txtdate)
+
+        }
+    }
 }
