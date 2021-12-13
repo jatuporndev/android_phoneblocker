c91af95442e9fcad4de64ea67c8e6469c2d00e9f
pumpunya <pumpunya1942@gmail.com>
2021-12-13 15:39:21 +0700

    โชว์ WifiFragment.kt

--- app/src/main/java/com/example/phoneblockerproject/Fragment/WifiFragment.kt
+++ app/src/main/java/com/example/phoneblockerproject/Fragment/WifiFragment.kt
@@ -28,6 +28,7 @@ import android.net.LinkAddress
 import android.net.Network
 import android.net.NetworkCapabilities
 import android.os.Build
+import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import java.net.Inet4Address
@@ -37,6 +38,19 @@ import java.util.*
 
 @RequiresApi(Build.VERSION_CODES.R)
 class WifiFragment : Fragment() {
+    var txtssid : TextView?=null
+    var txthidden: TextView?=null
+    var txtsignal: TextView?=null
+    var txtfrequency: TextView?=null
+    var txtlink: TextView?=null
+    var txtip: TextView?=null
+    var txtnetwork: TextView?=null
+    var txtgateway: TextView?=null
+    var txtdns: TextView?=null
+    var txtdomains: TextView?=null
+    var txthttp: TextView?=null
+    var txtnetworkmet: TextView?=null
+    var txtipv6: TextView?=null
 
 
     override fun onCreateView(
@@ -45,6 +59,20 @@ class WifiFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val root =inflater.inflate(R.layout.fragment_wifi, container, false)
+        txtssid=root.findViewById(R.id.txtssid)
+        txthidden=root.findViewById(R.id.txthidden)
+        txtsignal=root.findViewById(R.id.txtsignal)
+        txtfrequency=root.findViewById(R.id.txtfrequency)
+        txtlink=root.findViewById(R.id.txtlink)
+        txtip=root.findViewById(R.id.txtip)
+        txtnetwork=root.findViewById(R.id.txtnetwork)
+        txtgateway=root.findViewById(R.id.txtgateway)
+        txtdns=root.findViewById(R.id.txtdns)
+        txtdomains=root.findViewById(R.id.txtdomains)
+        txthttp=root.findViewById(R.id.txthttp)
+        txtnetworkmet=root.findViewById(R.id.txtnetworkmet)
+        txtipv6=root.findViewById(R.id.txtipv6)
+
         onReviewPermissions()
 
         return root
@@ -90,23 +118,38 @@ class WifiFragment : Fragment() {
             .sortedWith(InetAddressComparator).joinToString(separator = "\n") { it.format() }
 
         //ดูส่วนนี้ แล้วนำไปโชว์
-        Log.d("ssid", ssid)
-        Log.d("hiden_ssidd", hiden_ssidd)
-        Log.d("signal_level", signal_level)
-        Log.d("Frequency", Frequency)
-        Log.d("linkSpeed", linkSpeed)
-        Log.d("ipAddress", ipAddress)
-        Log.d("network_mask", network_mask)
-        Log.d("gatewayAddress", gatewayAddress)
-        Log.d("dnsServers", dnsServers[0].toString()+"|| "+dnsServers[1].toString())
+       // Log.d("ssid", ssid)
+        //Log.d("hiden_ssidd", hiden_ssidd)
+        //Log.d("signal_level", signal_level)
+        // Log.d("Frequency", Frequency)
+        //Log.d("linkSpeed", linkSpeed)
+        // Log.d("ipAddress", ipAddress)
+        // Log.d("network_mask", network_mask)
+        //Log.d("gatewayAddress", gatewayAddress)
+        // Log.d("dnsServers", dnsServers.toString())
+        txtssid?.text=ssid
+        txthidden?.text=hiden_ssidd
+        txtsignal?.text=signal_level
+        txtfrequency?.text=Frequency
+        txtlink?.text=linkSpeed
+        txtip?.text=ipAddress
+        txtnetwork?.text=network_mask
+        txtgateway?.text=gatewayAddress
+        txtdns?.text=dnsServers.toString()
+
         if (domains.isNullOrEmpty()){
+
             Log.d("domains", "none")
         }else{
             Log.d("domains", domains[0])
         }
-        Log.d("httpProxy", httpProxy)
-        Log.d("networkMetered", networkMetered)
-        Log.d("ipv6", ipv6)
+
+        txthttp?.text=httpProxy
+        txtnetworkmet?.text=networkMetered
+       // Log.d("httpProxy", httpProxy)
+       // Log.d("networkMetered", networkMetered)
+        txtipv6?.text=ipv6
+       // Log.d("ipv6", ipv6)
     }
     private fun onReviewPermissions() {
 
