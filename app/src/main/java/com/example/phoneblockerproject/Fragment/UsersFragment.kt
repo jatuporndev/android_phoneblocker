7fc54c88af4149004a8280826ea09f4c9a2f5d99
suwatnan <suwatnan2543@gmail.com>
2022-01-20 13:57:32 +0700

    edit UsersFragment.kt

--- app/src/main/java/com/example/phoneblockerproject/Fragment/UsersFragment.kt
+++ app/src/main/java/com/example/phoneblockerproject/Fragment/UsersFragment.kt
@@ -5,10 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.phoneblockerproject.R
 
 
 class UsersFragment : Fragment() {
+
+    var consignin: ConstraintLayout?=null
+    var conpackage: ConstraintLayout?=null
+    var condefense: ConstraintLayout?=null
+    var conterms: ConstraintLayout?=null
+    var consafety: ConstraintLayout?=null
+    var conregister: ConstraintLayout?=null
+    var imgback: ImageView?=null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,6 +26,39 @@ class UsersFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         var root =  inflater.inflate(R.layout.fragment_users, container, false)
+
+        imgback = root.findViewById(R.id.imgback)
+        imgback?.setOnClickListener {
+            val fragmentTransaction = requireActivity().supportFragmentManager
+            fragmentTransaction.popBackStack()
+        }
+
+        consignin = root.findViewById(R.id.consignin)
+
+        conpackage = root.findViewById(R.id.conpackage)
+
+        condefense = root.findViewById(R.id.condefense)
+        condefense?.setOnClickListener{
+            val fragmentTransaction = requireActivity().
+            supportFragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.nav_host_fragment, SettingsFragment())
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
+        }
+
+        conterms = root.findViewById(R.id.conterms)
+
+        consafety = root.findViewById(R.id.consafety)
+        consafety?.setOnClickListener {
+            val fragmentTransient = requireActivity().supportFragmentManager.beginTransaction()
+            fragmentTransient.replace(R.id.nav_host_fragment,SecurityFragment())
+            fragmentTransient.addToBackStack(null)
+            fragmentTransient.commit()
+
+        }
+        conregister = root.findViewById(R.id.conregister)
+
+
 
         return root
     }
