19dc297dd547e6faaa002a79a5fa9c5e7f343614
suwatnan <suwatnan2543@gmail.com>
2022-01-21 12:10:46 +0700

    add RegisterActivity.kt and design

--- app/src/main/java/com/example/phoneblockerproject/Fragment/RegisterActivity.kt
+++ app/src/main/java/com/example/phoneblockerproject/Fragment/RegisterActivity.kt
@@ -1,12 +1,22 @@
 package com.example.phoneblockerproject.Fragment
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.example.phoneblockerproject.R
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+
+        val txtbacklogin = findViewById<TextView>(R.id.txtbacklogin)
+        txtbacklogin.setOnClickListener{
+            val intent = Intent(applicationContext, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
