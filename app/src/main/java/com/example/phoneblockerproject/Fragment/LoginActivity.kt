f2ad08a1ead65bcc5e0f4590480b4aca9b8c5c7b
suwatnan <suwatnan2543@gmail.com>
2022-01-24 16:09:07 +0700

    add code login

--- app/src/main/java/com/example/phoneblockerproject/Fragment/LoginActivity.kt
+++ app/src/main/java/com/example/phoneblockerproject/Fragment/LoginActivity.kt
@@ -1,14 +1,27 @@
 package com.example.phoneblockerproject.Fragment
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.*
 import com.example.phoneblockerproject.MainActivity
 import com.example.phoneblockerproject.R
+import okhttp3.FormBody
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.IOException
 
 class LoginActivity : AppCompatActivity() {
+
+    val appPreference:String = "appPrefer"
+    val memberIdPreference:String = "memberIdPref"
+    val usernamePreference:String = "usernamePref"
+    val userstatus:String ="0"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -28,6 +41,61 @@ class LoginActivity : AppCompatActivity() {
             finish()
         }
 
+        val editUsername = findViewById<EditText>(R.id.editTextName)
+        val editPassword = findViewById<EditText>(R.id.editTextPassword)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+
+            val url = getString(R.string.root_url) + getString(R.string.login_url)
+            val okHttpClient = OkHttpClient()
+
+            val formBody: RequestBody = FormBody.Builder()
+                .add("username", editUsername.text.toString())
+                .add("password", editPassword.text.toString())
+                .build()
+            val request: Request = Request.Builder()
+                .url(url)
+                .post(formBody)
+                .build()
+            try {
+                val response = okHttpClient.newCall(request).execute()
+                if (response.isSuccessful) {
+                    try {
+                        val obj = JSONObject(response.body!!.string())
+                        val userID = obj["id"].toString()
+                        val username = obj["username"].toString()
+
+                        //Create shared preference to store user data
+                        val sharedPrefer: SharedPreferences =
+                            getSharedPreferences(appPreference, Context.MODE_PRIVATE)
+                        val editor: SharedPreferences.Editor = sharedPrefer.edit()
+
+                        editor.putString(memberIdPreference, userID)
+                        editor.putString(usernamePreference, username)
+                        editor.putString(userstatus, "0")
+                        editor.commit()
+
+                        //return to login page
+                            val intent = Intent(applicationContext, MainActivity::class.java)
+                            startActivity(intent)
+                            finish()
+
+
+                    } catch (e: JSONException) {
+                        e.printStackTrace()
+                    }
+                } else {
+                    response.code
+                    Toast.makeText(applicationContext, "ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง", Toast.LENGTH_LONG).show()
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+
+
     }
+
 
 }
--- app/src/main/java/com/example/phoneblockerproject/Fragment/UsersFragment.kt
+++ app/src/main/java/com/example/phoneblockerproject/Fragment/UsersFragment.kt
@@ -1,64 +1,116 @@
 package com.example.phoneblockerproject.Fragment
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.phoneblockerproject.MainActivity
 import com.example.phoneblockerproject.R
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.IOException
+import kotlin.math.log
 
 
 class UsersFragment : Fragment() {
 
     var consignin: ConstraintLayout?=null
+    var conlogout: ConstraintLayout?=null
     var conpackage: ConstraintLayout?=null
     var condefense: ConstraintLayout?=null
     var conterms: ConstraintLayout?=null
     var consafety: ConstraintLayout?=null
     var imgback: ImageView?=null
-
+    var memberID:String?=null
+    var userstatus: String? = null
+    var txtusername: TextView? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        val sharedPrefer = requireContext().getSharedPreferences(
+            LoginActivity().appPreference, Context.MODE_PRIVATE)
+        userstatus = sharedPrefer?.getString(LoginActivity().userstatus, null)
+        memberID = sharedPrefer?.getString(LoginActivity().memberIdPreference, null)
+
+
         var root =  inflater.inflate(R.layout.fragment_users, container, false)
-
+        txtusername = root.findViewById(R.id.txtusername)
         imgback = root.findViewById(R.id.imgback)
-        imgback?.setOnClickListener {
-            val fragmentTransaction = requireActivity().supportFragmentManager
-            fragmentTransaction.popBackStack()
-        }
-
         consignin = root.findViewById(R.id.consignin)
-        consignin?.setOnClickListener{
-            startActivity(Intent(requireContext(), LoginActivity::class.java))
-        }
-
         conpackage = root.findViewById(R.id.conpackage)
-        conpackage?.setOnClickListener{
-            val fragmentTransaction = requireActivity().
-            supportFragmentManager.beginTransaction()
-            fragmentTransaction.replace(R.id.nav_host_fragment, PackageFragment())
-            fragmentTransaction.addToBackStack(null)
-            fragmentTransaction.commit()
-        }
         condefense = root.findViewById(R.id.condefense)
-        condefense?.setOnClickListener{
+        conterms = root.findViewById(R.id.conterms)
+        consafety = root.findViewById(R.id.consafety)
+        conlogout = root.findViewById(R.id.conlogout)
+
+        consignin?.visibility = View.VISIBLE
+        if (userstatus == "0") {
+          consignin?.visibility = View.GONE
+
+        }else {
+            conlogout?.visibility = View.GONE
+        }
+        viewmember(memberID)
+        return root
+
+    }
+
+    private fun viewmember(memberID: String?) {
+        var url: String = getString(R.string.root_url) + getString(R.string.viewmember_url) + memberID
+        val okHttpClient = OkHttpClient()
+        val request: Request = Request.Builder()
+            .url(url)
+            .get()
+            .build()
+
+        try {
+            val response = okHttpClient.newCall(request).execute()
+            if (response.isSuccessful) {
+                try {
+                    val data = JSONObject(response.body!!.string())
+                    if (data.length() > 0) {
+
+                        txtusername?.text = data.getString("username")
+                    }
+
+                } catch (e: JSONException) {
+                    e.printStackTrace()
+                }
+            } else {
+                response.code
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+            // ออกจากระบบ
+        conlogout?.setOnClickListener {
+            val sharePrefer = requireContext().getSharedPreferences(
+                LoginActivity().appPreference,
+                Context.MODE_PRIVATE
+            )
+            val editor = sharePrefer.edit()
+            editor.clear() // ทำการลบข้อมูลทั้งหมดจาก preferences
+
+            editor.commit() // ยืนยันการแก้ไข preferences
+            //return to login page
             val fragmentTransaction = requireActivity().
             supportFragmentManager.beginTransaction()
-            fragmentTransaction.replace(R.id.nav_host_fragment, SettingsFragment())
+            fragmentTransaction.replace(R.id.nav_host_fragment, HomeFragment())
             fragmentTransaction.addToBackStack(null)
             fragmentTransaction.commit()
         }
-
-        conterms = root.findViewById(R.id.conterms)
-
-        consafety = root.findViewById(R.id.consafety)
+            // ตั้งค่าความปลอดภัย
         consafety?.setOnClickListener {
             val fragmentTransient = requireActivity().supportFragmentManager.beginTransaction()
             fragmentTransient.replace(R.id.nav_host_fragment,SecurityFragment())
@@ -66,11 +118,32 @@ class UsersFragment : Fragment() {
             fragmentTransient.commit()
 
         }
+            // ตั้งค่า
+        condefense?.setOnClickListener{
+            val fragmentTransaction = requireActivity().
+            supportFragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.nav_host_fragment, SettingsFragment())
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
+        }
+            // ซ์้อแพ็กแก็จ
+        conpackage?.setOnClickListener{
+            val fragmentTransaction = requireActivity().
+            supportFragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.nav_host_fragment, PackageFragment())
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
+        }
+            // ล็อกอิน
+        consignin?.setOnClickListener{
+            startActivity(Intent(requireContext(), LoginActivity::class.java))
+        }
+            // กลับ
+        imgback?.setOnClickListener {
+            val fragmentTransaction = requireActivity().supportFragmentManager
+            fragmentTransaction.popBackStack()
+        }
 
-
-
-
-        return root
     }
 
 
